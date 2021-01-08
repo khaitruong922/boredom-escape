93df6073ec6396f29b0e28a35f275b9fc93090b5
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-01-08 22:35:05 +0700

    finish main menu

---
+++ sources/Assets/Scripts/MoveComponent.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable 0649
+
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class MoveComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float baseSpeed = 5f;
+    private float multiplier = 1f;
+    public float Multiplier { get => multiplier; set => multiplier = value; }
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Move(Vector2 direction)
+    {
+        rb.velocity = direction * baseSpeed * multiplier;
+    }
+}
+
