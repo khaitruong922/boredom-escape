486cb490ce964c8acb205aacb001b68e8b3ef658
Khai Truong <kd.kidr@gmail.com>
2021-01-09 19:56:45 +0700

    update task view

--- sources/Assets/Scripts/TaskView.cs
+++ sources/Assets/Scripts/TaskView.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 public class TaskView : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI titleText, descriptionText, durationText, cooldownText, energyText, healthText, pointText, finishedText;
-    
+    private TextMeshProUGUI titleText, descriptionText, durationText, cooldownText, energyText, healthText, scoreText;
+
     public void DisplayTask(Task task)
     {
         titleText.text = task.title;
@@ -17,10 +17,7 @@ public class TaskView : MonoBehaviour
         cooldownText.text = task.cooldown.ToString();
         cooldownText.text = task.energy.ToString();
         energyText.text = task.health.ToString();
-        // pointText.text = task.point.ToString();
-
-
-
+        scoreText.text = task.score.ToString();
     }
 }
 
