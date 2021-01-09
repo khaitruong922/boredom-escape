b0e5acd6b9c2474fe9e39a5e09ae0b7b25daeb72
Khai Truong <kd.kidr@gmail.com>
2021-01-09 20:02:07 +0700

    fix canvas order

--- sources/Assets/Scripts/TaskView.cs
+++ sources/Assets/Scripts/TaskView.cs
@@ -7,16 +7,15 @@ using UnityEngine;
 public class TaskView : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI titleText, descriptionText, durationText, cooldownText, energyText, healthText, scoreText;
+    private TextMeshProUGUI titleText, descriptionText, durationText, energyText, healthText, scoreText;
 
     public void DisplayTask(Task task)
     {
         titleText.text = task.title;
         descriptionText.text = task.description;
         durationText.text = task.duration.ToString();
-        cooldownText.text = task.cooldown.ToString();
-        cooldownText.text = task.energy.ToString();
-        energyText.text = task.health.ToString();
+        healthText.text = task.health.ToString();
+        energyText.text = task.energy.ToString(); 
         scoreText.text = task.score.ToString();
     }
 }
