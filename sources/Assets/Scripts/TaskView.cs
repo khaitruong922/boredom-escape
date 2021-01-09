e7c0baa5ab85178e1e4df2f9f138efe3f2480d81
Nam Pham <nam.ashton@gmail.com>
2021-01-09 13:21:30 +0700

    Taskview added

---
+++ sources/Assets/Scripts/TaskView.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TaskView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI titleText, descriptionText, durationText, cooldownText, energyText, healthText;
+    
+    public void DisplayTask(Task task)
+    {
+        titleText.text = task.title;
+        descriptionText.text = task.description;
+        durationText.text = task.duration.ToString();
+        cooldownText.text = task.cooldown.ToString();
+        cooldownText.text = task.energy.ToString();
+        energyText.text = task.health.ToString();
+
+    }
+}
+
