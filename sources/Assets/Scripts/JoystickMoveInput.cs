b2e0eeb70fbe0349ee9ba969f17a8221dce8fe1f
Khai Truong <kd.kidr@gmail.com>
2021-01-09 10:52:34 +0700

    joystick movement

---
+++ sources/Assets/Scripts/JoystickMoveInput.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MoveComponent))]
+public class JoystickMoveInput : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick joystick;
+    private MoveComponent moveComponent;
+    private void Awake()
+    {
+        moveComponent = GetComponent<MoveComponent>();
+    }
+    void Update()
+    {
+        moveComponent.Move(new Vector2(joystick.Horizontal, joystick.Vertical).normalized);
+    }
+}
