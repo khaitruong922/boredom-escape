412293a582bbf15056575bc790956fd4150d9f05
Khai Truong <kd.kidr@gmail.com>
2021-01-10 13:09:25 +0700

    color task

--- sources/Assets/Scripts/TaskView.cs
+++ sources/Assets/Scripts/TaskView.cs
@@ -7,16 +7,36 @@ using UnityEngine;
 public class TaskView : MonoBehaviour
 {
     [SerializeField]
+    private Color positiveColor, neutralColor, negativeColor;
+    [SerializeField]
     private TextMeshProUGUI titleText, descriptionText, durationText, energyText, healthText, scoreText;
 
     public void DisplayTask(Task task)
     {
         titleText.text = task.title;
         descriptionText.text = task.description;
+
         durationText.text = task.duration.ToString();
-        healthText.text = task.health.ToString();
-        energyText.text = task.energy.ToString(); 
-        scoreText.text = task.score.ToString();
+        durationText.color = neutralColor;
+
+        healthText.text = GetStatString(task.health);
+        healthText.color = GetDisplayColor(task.health);
+
+        energyText.text = GetStatString(task.energy);
+        energyText.color = GetDisplayColor(task.energy);
+
+        scoreText.text = GetStatString(task.score);
+        scoreText.color = GetDisplayColor(task.score);
+    }
+    private string GetStatString(float value)
+    {
+        return (value >= 0 ? "+" : "") + value.ToString();
+    }
+    private Color GetDisplayColor(float value)
+    {
+        if (value > 0) return positiveColor;
+        if (value == 0) return neutralColor;
+        return negativeColor;
     }
 }
 
